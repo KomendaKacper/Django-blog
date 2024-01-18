cbd55954c2f858f7c4541fdd517ac1c148f48d15
KomendaKacper <komenda@student.agh.edu.pl>
2024-01-18 12:54:11 +0100

    UserCreate

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('', views.home, name="main-home"),
     path('about/', views.about, name="main-about"),
     path('home/', views.home, name="main-home"),
+    path('register/', user_views.register, name="register"),
 ]
