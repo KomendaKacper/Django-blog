bc258986d7d19db96e54e48dc6facfd1ea1c11fb
KomendaKacper <komenda@student.agh.edu.pl>
2024-01-19 20:20:38 +0100

    Logout

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth import logout
 
 def register(request):
     if request.method == "POST":
@@ -9,8 +10,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('main-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'users/logout.html')
