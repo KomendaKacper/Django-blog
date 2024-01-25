269bb92bf14991f6750c7a2eee1d2f7ebfc9dab7
KomendaKacper <komenda@student.agh.edu.pl>
2024-01-25 23:56:05 +0100

    Modyfikacja postów

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
+
 from . import views
 from users import views as user_views
 
 urlpatterns = [
-    path('', views.home, name="main-home"),
+    path('', PostListView.as_view(), name="main-home"),
     path('about/', views.about, name="main-about"),
     path('home/', PostListView.as_view(), name="main-home"),
     path('register/', user_views.register, name="register"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
-
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
+    
 ]
